651bde8c5c84e9c1f1f18dca70dcfe01e28634fc
joshuasilva414 <joshuasilva414@gmail.com>
2023-11-22 02:25:44 -0600

    Admin page authentication middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/challenges", "/leaderboard"],
+  afterAuth(auth, req, evt) {
+    if (
+      req.nextUrl.pathname === "/admin" &&
+      !auth.user?.privateMetadata?.isAdmin
+    ) {
+      return new NextResponse("Not authorized", { status: 403 });
+    } else {
+      return NextResponse.next();
+    }
+  },
 });
 
 export const config = {
